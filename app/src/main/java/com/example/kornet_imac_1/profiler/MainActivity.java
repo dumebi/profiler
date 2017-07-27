c6f8e324c63300475f3870d93b5a3a87d4cf4e4e
Jude Dike <dikejude49@gmail.com>
2017-07-27 12:52:44 +0100

    initial, styles and colors done

---
+++ app/src/main/java/com/example/kornet_imac_1/profiler/MainActivity.java
@@ -0,0 +1,13 @@
+ package com.example.kornet_imac_1.profiler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
